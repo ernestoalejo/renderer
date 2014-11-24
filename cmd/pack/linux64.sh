bca3eee9f5e11b4f3f9690dc2dc57144d872fbf2
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2014-11-24 08:18:35 +0100

    Script to generated packed compiled versions of the app.

---
+++ cmd/pack/linux64.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+set -u
+
+NAME=renderer-linux64
+DEST=/tmp/pack/$NAME
+VERSION=0.1.0
+
+# Build the binaries
+scons -c
+scons
+
+# Copy the files to an independent folder
+rm -rf $DEST
+mkdir -p $DEST
+cp bin/* $DEST
+cp -R /cef/* $DEST
+rm -r $DEST/include
+
+# Compress the pack with the files
+(cd /tmp/pack && tar -vpczf $NAME.tar.gz $NAME)
+
+# Move the packs to the correct folder versioning them
+rm -rf packs
+mkdir packs
+cd packs
+mv $DEST.tar.gz $NAME.$VERSION.tar.gz

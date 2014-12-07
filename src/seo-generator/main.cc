cacdd94d3d3d245c8d368ad4fa99a0dd52b70915
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2014-12-07 13:08:53 +0100

    Log creation of sample request file.

--- src/seo-generator/main.cc
+++ src/seo-generator/main.cc
@@ -29,6 +29,8 @@ int main(int argc, char* argv[]) {
     LOG(FATAL) << "failed to write request file";
   }
 
+  LOG(INFO) << "request generated in /tmp/request";
+
   // Shutdown protobuf
   google::protobuf::ShutdownProtobufLibrary();
 
